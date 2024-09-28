d664684b97bed2521969934c1165d53e23d43249
FellipeF <fellipefreire99@hotmail.com>
2024-09-28 20:02:08 -0300

    Service Layer and component registration

--- src/main/java/com/java/cursojava/resources/UserResource.java
+++ src/main/java/com/java/cursojava/resources/UserResource.java
@@ -1,26 +1,42 @@
 package com.java.cursojava.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.java.cursojava.entities.User;
+import com.java.cursojava.services.UserService;
 
 //Defines that this class is a Web Resource that is implemented by a Rest Controller
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
-	//Endpoint that access users
+
+	@Autowired
+	private UserService service;	
 	
 	//Returns responses from Web Requests
-
+	//Endpoint that access users
+	
 	@GetMapping	//Responds to HTTP GET requests
-	public ResponseEntity<User> findAll()
+	public ResponseEntity<List<User>> findAll()
 	{
-		//Begin testing
-		User u = new User(1L, "Maria", "maria@gmail.com", "9555-5555", "1147");
+		//Call to Service Layer
+		List <User> list = service.findAll();
+				
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id)
+	{
+		User u = service.findById(id);
 		return ResponseEntity.ok().body(u);
+		//TODO: Error Treating
 	}
 }
